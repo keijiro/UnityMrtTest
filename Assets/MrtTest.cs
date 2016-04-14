ec696e864b1f0608fff20d150ce26b5dd6631edd
Keijiro Takahashi <keijiro@ZX.local>
2016-04-14 17:34:36 +0900

    Initial commit

---
+++ Assets/MrtTest.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class MrtTest : MonoBehaviour
+{
+    [SerializeField] Shader _shader;
+
+    Material _material;
+    RenderBuffer[] _mrt;
+
+    void OnEnable()
+    {
+        var shader = Shader.Find("Hidden/MrtTest");
+        _material = new Material(shader);
+        _material.hideFlags = HideFlags.DontSave;
+        _mrt = new RenderBuffer[2];
+    }
+
+    void OnDisable()
+    {
+        DestroyImmediate(_material);
+        _material = null;
+        _mrt = null;
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        var rt1 = RenderTexture.GetTemporary(source.width, source.height);
+        var rt2 = RenderTexture.GetTemporary(source.width, source.height);
+
+        _mrt[0] = rt1.colorBuffer;
+        _mrt[1] = rt2.colorBuffer;
+
+        // Blit with a MRT.
+        Graphics.SetRenderTarget(_mrt, rt1.depthBuffer);
+        Graphics.Blit(null, _material, 0);
+
+        // Combine them and output to the destination.
+        _material.SetTexture("_SecondTex", rt1);
+        _material.SetTexture("_ThirdTex", rt2);
+        Graphics.Blit(source, destination, _material, 1);
+
+        RenderTexture.ReleaseTemporary(rt1);
+        RenderTexture.ReleaseTemporary(rt2);
+    }
+
+    void OnGUI()
+    {
+        var text = "Supported MRT count: ";
+        text += SystemInfo.supportedRenderTargetCount;
+        GUI.Label(new Rect(0, 0, 200, 200), text);
+    }
+}
---
+++ Assets/MrtTest.shader
@@ -0,0 +1,62 @@
+﻿Shader "Hidden/MrtTest"
+{
+    Properties
+    {
+        _MainTex("", 2D) = "white"{}
+        _SecondTex("", 2D) = "white"{}
+        _ThirdTex("", 2D) = "white"{}
+    }
+
+    CGINCLUDE
+
+    #include "UnityCG.cginc"
+
+    sampler2D _MainTex;
+    sampler2D _SecondTex;
+    sampler2D _ThirdTex;
+
+    // MRT shader
+    struct FragmentOutput
+    {
+        half4 dest0 : SV_Target0;
+        half4 dest1 : SV_Target1;
+    };
+
+    FragmentOutput frag_mrt(v2f_img i) : SV_Target
+    {
+        FragmentOutput o;
+        o.dest0 = frac(i.uv.x * 10);
+        o.dest1 = frac(i.uv.y * 10);
+        return o;
+    }
+
+    // Simple combiner
+    half4 frag_combine(v2f_img i) : SV_Target
+    {
+        half4 t1 = tex2D(_MainTex, i.uv);
+        half4 t2 = tex2D(_SecondTex, i.uv);
+        half4 t3 = tex2D(_ThirdTex, i.uv);
+        return half4(t1.r, t2.g, t3.b, 1);
+    }
+
+    ENDCG
+
+    SubShader
+    {
+        Cull Off ZWrite Off ZTest Always
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag_mrt
+            ENDCG
+        }
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag_combine
+            ENDCG
+        }
+    }
+}
